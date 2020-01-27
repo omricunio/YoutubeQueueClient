cfb7cff87ab272b97233c377582b0a4a03751b5b
Omri Cunio <omricunio@gmail.com>
2020-01-27 19:13:51 +0200

    Added simple item

---
+++ src/components/QueueItem/QueueItem.jsx
@@ -0,0 +1,37 @@
+import React, { Component } from 'react'
+import { Card, CardHeader, Avatar, IconButton, CardMedia, CardContent, Typography } from '@material-ui/core'
+import { withStyles } from '@material-ui/core/styles';
+import styles from './styles';
+
+class QueueItem extends Component {
+    render() {
+        const { classes } = this.props;
+        return (
+            <Card>
+                <CardHeader
+                    avatar={
+                    <Avatar aria-label="recipe" /*className={classes.avatar}*/>
+                        R
+                    </Avatar>
+                    }
+                    //action={}
+                    title="Shrimp and Chorizo Paella"
+                    subheader="September 14, 2016"
+                />
+                <CardMedia
+                    className={classes.image}
+                    image="https://i.ytimg.com/vi/ia1iuXbEaYQ/hqdefault.jpg"
+                    title="Paella dish"
+                />
+                <CardContent>
+                    <Typography variant="body2" color="textSecondary" component="p">
+                    This impressive paella is a perfect party dish and a fun meal to cook together with your
+                    guests. Add 1 cup of frozen peas along with the mussels, if you like.
+                    </Typography>
+                </CardContent>                
+            </Card>
+        )
+    }
+}
+
+export default withStyles(styles)(QueueItem);
