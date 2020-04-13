76bea876649b858b4cec1725659f6c78da4a69e2
Omri Cunio <your_email@example.com>
2020-04-13 13:26:18 +0300

    changed spinning image to disc

--- src/components/SpinningImage/styles.js
+++ src/components/SpinningImage/styles.js
@@ -2,7 +2,18 @@ export default function styles(theme) {
     return {
         root: {
             clipPath: "circle(45% at center)",
-            animation: "$spin 5s infinite linear"
+            animation: "$spin 5s infinite linear",
+            position: "relative"
+        },
+        disc: {
+            width: "100%"
+        },
+        image: {
+            position: "absolute",
+            top: "0px",
+            width: "100%",
+            transform: "scale(0.5)",
+            clipPath: "circle(45% at center)"
         },
         "@keyframes spin": {
             from: {
