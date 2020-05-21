197a83da25661a3a72cf29ca4cd5c4439030c8fd
Omri Cunio <your_email@example.com>
2020-05-21 18:04:52 +0300

    changed server host

--- src/config/hosts.js
+++ src/config/hosts.js
@@ -1,2 +1,2 @@
 export const YOUTUBE_API_URL = "https://content.googleapis.com/youtube/v3/";
-export const SERVER_URL = "http://10.0.0.10:8080/"
+export const SERVER_URL = "https://youtube-queue-server.herokuapp.com/"
