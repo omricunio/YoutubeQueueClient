18ef6a326e3afacab64ff12ccc7388db3b38f3dc
Omri Cunio <your_email@example.com>
2020-05-22 12:14:39 +0300

    auto play fix

--- src/utils/socketListener.js
+++ src/utils/socketListener.js
@@ -1,7 +1,8 @@
 import socketIOClient from "socket.io-client";
 import { SERVER_URL } from "../config/hosts";
 import { store } from "../index";
-import { addItem, deleteItemByIndex } from "../reducers/player/actions";
+import { deleteItemByIndex } from "../reducers/player/actions";
+import { addItemMiddleware } from "../sagas/player/actions";
 
 export async function listenToQueueEvents(queueGuid) {
   console.log(`Start listening to queue ${queueGuid}`);
@@ -9,7 +10,7 @@ export async function listenToQueueEvents(queueGuid) {
     query: `queueGuid=${queueGuid}`
   });
   socket.on("added", (item) => {
-    store.dispatch(addItem(item));
+    store.dispatch(addItemMiddleware(item));
   });
   socket.on("removed", (index) => {
     store.dispatch(deleteItemByIndex(Number(index)));
