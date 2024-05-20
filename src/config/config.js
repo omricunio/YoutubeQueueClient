6dcceb9bfb8dd66191627efbbb5991db28b232bf
Omri Cunio <31957947+omricunio@users.noreply.github.com>
2024-05-20 14:20:06 +0300

    Update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,5 @@
 export const YOUTUBE_API_URL = "https://content.googleapis.com/youtube/v3/";
-export const SERVER_URL = "https://youtube-queue-server.herokuapp.com/";
+export const SERVER_URL = "https://youtube-queue-server-b5002cb0b521.herokuapp.com";
 
 export const VOLUME_CHANGE_PERCENTAGE = 5;
 export const PROGRESS_CHANGE_PERCENTAGE = 2;
